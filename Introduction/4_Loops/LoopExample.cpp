751c0a9b796b9ebf8f2aced32fce4c7df94655dd
Levi-Nicklas <levi.nicklas@gmail.com>
2020-05-21 18:20:46 -0400

    Add Loop Example

---
+++ Introduction/4_Loops/LoopExample.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //For loop example
+    for(int i = 0; i<10; i++){
+        cout << i << "   ";
+    }
+
+    cout << endl;
+
+    //While Loop Example
+    int j = 1;
+
+    while(j <10){
+        cout << j << "-";
+        j++;
+    }
+
+
+    return 0;
+}
